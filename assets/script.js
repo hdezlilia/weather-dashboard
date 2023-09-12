29d7c95c48f61dfc141ab5fad8ec1799a2f5b01b
hdezlilia <127769063+hdezlilia@users.noreply.github.com>
2023-09-11 21:58:54 -0700

    adding javascript

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var searchBtnEl = document.getElementById('search-btn');
+var citySearch = document.getElementById('search-input');
+var stateSearch = document.getElementById('state-search');
+var forecastArea = document.getElementById('five-day');
+var prevSearch = document.getElementById('prev-search');
+var prevBtn;
+let activateBtn = document.getElementsByClassName('btn')
+// needs to add var for API..
+
+// latitude and longtitude of the city
+
+
+// weather longtitude + latitude 
